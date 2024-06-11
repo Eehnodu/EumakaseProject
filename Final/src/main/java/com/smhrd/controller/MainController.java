4fb714ecb2396e74790752af2dcc70f1af69c830
Nodu <Nodu00719@gmail.com>
2024-06-12 01:44:59 +0900

    곡 상세 페이지 구현

--- Final/src/main/java/com/smhrd/controller/MainController.java
+++ Final/src/main/java/com/smhrd/controller/MainController.java
@@ -790,9 +790,12 @@ public class MainController {
 	}
 	
 	@GetMapping("/songDetail")
-	public String songDetail() {
-		return "songDetail";
-		
+	public String songDetail(@RequestParam int musicIdx, Model model) {
+	    // musicIdx를 이용하여 해당 곡의 상세 정보를 가져옵니다.
+	    MusicVO music = musicMapper.getUserPlaylist(musicIdx);
+	    model.addAttribute("musicDetail", music);
+	    return "songDetail";
 	}
+
 
 }
