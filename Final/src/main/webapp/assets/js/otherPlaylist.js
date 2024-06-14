2eb991cc21543f101b8fc84a615d91f31387baa6
엄희수 <lacholla124@gmail.com>
2024-06-14 09:36:42 +0900

    이 플리 저장하기 / 다른 장르 추천 디자인 수정

--- Final/src/main/webapp/assets/js/otherPlaylist.js
+++ Final/src/main/webapp/assets/js/otherPlaylist.js
@@ -1,39 +1,47 @@
 const cpath = document.body.getAttribute('data-cpath');
 
 $(document).ready(function() {
-    $('button#redDiff').click(function(event) {
-        event.preventDefault(); // 링크 기본 동작 방지
+	$('button#redDiff').click(function(event) {
+		event.preventDefault(); // 링크 기본 동작 방지
+
+		// 토글 버튼 활성/비활성 상태 변경
+		$(this).addClass('active');
+		
+		// 일정 시간 후에 색상을 원래 상태로 되돌림
+		setTimeout(function() {
+			$('.toggle-btn').removeClass('active');
+		}, 300); 
 
 		var value = $(this).attr('value');
-        $.ajax({
-            type: "post",
-            url: `${cpath}/otherPlaylist`, // URL을 적절히 변경하세요
-            data : { value: value},
-            success: function(data) {
-                updatePlaylist(data);
-            },
-            error: function(e) {
-                console.log(e);
-            }
-        });
-    });
+		$.ajax({
+			type: "post",
+			url: `${cpath}/otherPlaylist`,
+			data: { value: value },
+			success: function(data) {
+				updatePlaylist(data);
+			},
+			error: function(e) {
+				console.log(e);
+			}
+		});
+	});
 });
 
 function formatTimestamp(timestamp) {
-    var date = new Date(parseInt(timestamp));
-    var year = date.getFullYear();
-    var month = ('0' + (date.getMonth() + 1)).slice(-2);
-    var day = ('0' + date.getDate()).slice(-2);
-    return year + '-' + month + '-' + day;
+	var date = new Date(parseInt(timestamp));
+	var year = date.getFullYear();
+	var month = ('0' + (date.getMonth() + 1)).slice(-2);
+	var day = ('0' + date.getDate()).slice(-2);
+	return year + '-' + month + '-' + day;
 }
 
 function updatePlaylist(musicList) {
-    const playlistContainer = $('#playlistList');
-    playlistContainer.empty(); // 기존 내용 지우기
+	const playlistContainer = $('#playlistList');
+	playlistContainer.empty(); // 기존 내용 지우기
 
-    musicList.forEach(function(music) {
-        const formattedDate = music.releasedAt === '0001-01-01' ? '-' : formatTimestamp(music.releasedAt);
-        const musicItem = `
+	musicList.forEach(function(music) {
+		const formattedDate = music.releasedAt === '0001-01-01' ? '-' : formatTimestamp(music.releasedAt);
+		const musicItem = `
             <div class="col-lg-12 playlist">
                 <div class="item songDetail" style="display:block;">
                     <ul>
@@ -57,6 +65,6 @@ function updatePlaylist(musicList) {
                 </div>
             </div>
         `;
-        playlistContainer.append(musicItem);
-    });
+		playlistContainer.append(musicItem);
+	});
 }
